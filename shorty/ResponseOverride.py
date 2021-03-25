7318a10837f65cde25420c9f276a1aa2b69bd9ce
Ioannis Gkinis <igkinis@omilia.com>
2021-03-25 18:56:03 +0200

    added more tests

--- shorty/ResponseOverride.py
+++ shorty/ResponseOverride.py
@@ -1,7 +1,7 @@
 import json
 
 
-class Response(Exception):
+class ResponseOverride(Exception):
     def __init__(self, status_code: int = None, url: str = None, link: str = None,
                  provider: str = None, message: str = None):
         self.status_code = status_code
@@ -10,13 +10,12 @@ class Response(Exception):
         self.provider = provider
         self.message = message
         self.attributes = {"status_code": self.status_code, "url": self.url, "link": self.text,
-                                "provider": self.provider, "message": self.message}
+                           "provider": self.provider, "message": self.message}
         self.attributes_str = json.dumps(self.attributes)
 
     def json(self):
         return json.loads(self.attributes_str)
 
     def to_dict(self):
-
         return {"status_code": self.status_code, "url": self.url, "link": self.text,
                 "provider": self.provider, "message": self.message}
